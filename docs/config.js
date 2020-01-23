f223c5b956aa5a5297afbe3c1a6ee0d462d27a8b
AlejandroParada <45214833+AlejandroParada@users.noreply.github.com>
2020-01-23 00:37:26 -0300

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,10 @@
+//config.js
+module.exports = {
+    title: 'Preetish HS',
+    description: 'My awesome portfolio blog',
+    themeConfig: {
+        sidebar: [
+          '/ConsejosDeSeguridad'        
+        ]
+    }
+  }
