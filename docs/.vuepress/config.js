65cc81e8edfea2cd78a1de3759b183161c269ce1
Alejandro Parada <alejandaroptaib@gmail.com>
2020-01-27 15:30:59 -0300

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+//esto va en .vuepress/config.js
+module.exports = {
+    base: '/Proyecto_SI/',
+    title: 'Preetish HS',
+    description: 'My awesome portfolio blog',
+    themeConfig: {
+        sidebar: [
+          '/ConsejosDeSeguridad'        
+        ]
+    }
+  }
