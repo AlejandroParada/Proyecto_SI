76080337d28b51e0cd1d3f8c8a4407a61b4bf404
Alejandro Parada <alejandropta@gmail.com>
2020-01-31 01:12:19 -0300

    actualizacion

--- deploy.cmd
+++ deploy.cmd
@@ -3,11 +3,11 @@ git add .
 git commit -m "deploy"
 git push
 
-xcopy /S /Y /-Y docs\.vuepress\dist D:\temp\site
+xcopy /S /Y docs\.vuepress\dist D:\temp\site
 rem git pull
 git checkout gh-pages
 git pull
-xcopy /S /Y /-Y D:\temp\site D:\ale\codigo\Proyecto_SI 
+xcopy /S /Y D:\temp\site D:\ale\codigo\Proyecto_SI 
 git add .
 git commit -m "deploy"
 git push
