cb603f2da9dbbebf40941c1a00306644ee4203ab
Alejandro Parada <alejandropta@gmail.com>
2020-01-31 01:31:11 -0300

    deploy

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,10 @@
-git checkout master
-npm run build
+rem npm run build
 git add .
 git commit -m "deploy"
 git push
 
 xcopy /S /Y docs\.vuepress\dist D:\temp\site
 rem git pull
-
-echo gh-pages
 git checkout gh-pages
 git pull
 xcopy /S /Y D:\temp\site D:\ale\codigo\Proyecto_SI 
